727503e326188c950decf4a0cc49a17f136e8109
Brigid Burns <bburns4@student11.cse.nd.edu>
2023-06-05 12:42:37 -0400

    Trying to fix check_differences.py

--- check_differences.py
+++ check_differences.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 '''
 This Python script is used to compare the original .frd files to the files processed using the ASPEN
 software directly. 
@@ -16,7 +18,7 @@ frd_path = '/escnfs/home/bburns4/richter-lab/kyle2008/esure_summer2023/frd/'
 # but can I access it if I'm still on this branch? Idk something to look into 
 # processed_path = '/escnfs/home/bburns4/richter-lab/kyle2008/esure_summer2023'
 # frd_path = '/escnfs/home/bburns4/richter-lab/kyle2008/esure_summer2023'
-
+'''
 for processed_file in os.listdir(processed_path):
     print(processed_file)
     processed = os.path.splitext(processed_file)[0]
@@ -52,3 +54,34 @@ for processed_file in os.listdir(processed_path):
                 if error:
                     print("Error:")
                     print(error.decode())
+'''
+
+processed_file = '/escnfs/home/bburns4/richter-lab/kyle2008/esure_summer2023/processed/test2.txt'
+frd_file = '/escnfs/home/bburns4/richter-lab/kyle2008/esure_summer2023/frd/test.txt'
+
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_processed_file, tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_frd_file:
+    with open(processed_file, 'r') as orig_processed_file, open(frd_file, 'r') as orig_frd_file:
+        # Skip the first 3 lines and write the rest to temporary files
+        temp_processed_file.writelines(orig_processed_file.readlines()[3:])
+        temp_frd_file.writelines(orig_frd_file.readlines()[3:])
+
+        temp_processed_file.flush()
+        temp_frd_file.flush()
+
+        # Specify the command with the temporary file paths
+        command = ['diff', '-w', temp_processed_file.name, temp_frd_file.name]
+    
+        # Execute the command in a subprocess
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Wait for the command to finish and capture the output
+        output, error = process.communicate()
+    
+        # Print the output
+        if output:
+            print("Output:")
+            print(output.decode())
+
+        # Print any error messages
+        if error:
+            print("Error:")
+            print(error.decode())
