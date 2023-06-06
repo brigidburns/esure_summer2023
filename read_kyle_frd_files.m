f00be324deb460637c93a633f8cb5f5ab7282c53
Brigid Burns <bburns4@student11.cse.nd.edu>
2023-06-06 09:19:05 -0400

    Updates to read_kyle_frd_files.m

--- read_kyle_frd_files.m
+++ read_kyle_frd_files.m
@@ -5,21 +5,20 @@ close all
 % Hurricane: Kyle 2008
 
 sonde_idx = 1;
-filedirtmp = "C:\Users\brigi\OneDrive\Desktop\Kyle2008\Kyle2008_Processed\"; 
+filedirtmp = './Kyle2008_Processed/';
 file_list = dir(filedirtmp); 
+disp(file_list); 
 
-for i = 1:length(file_list) % Loop every dropsonde
-    if ~file_list(i).isdir 
-        file_name = file_list(i).name;
-        file = strcat(filedirtmp, file_name); 
-        fprintf('Opening file: %s\n', file_name); 
-        file_name = 'C:\Users\brigi\OneDrive\Desktop\Kyle2008\Kyle2008_Processed\g053626065QC.frd'; 
+for i = 1:length(file_list)
+    if ~file_list(i).isdir
+        file_name = strcat(filedirtmp, file_list(i).name);
+        fprintf('Opening file: %s\n', file_list(i).name); 
         datstruct = importdata(file_name,' ',21); 
         frddat{sonde_idx} = datstruct.data; 
         sonde_idx = sonde_idx + 1; 
     end
 end
 
-filename = 'C:\Users\brigi\OneDrive\Desktop\Kyle2008\Kyle2008_Processed\Kyle2008_frd_files.mat';
-save(filename,'frddat');
+filename = './Kyle2008_Processed/Kyle2008_frd_files.mat'; 
+save(filename, 'frddat'); 
 clearvars frddat
